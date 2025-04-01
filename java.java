e926431767130406e0c094065bacad23450b776e
Sahana Patil <sahanapatil223@gmail.com>
2025-04-01 16:49:25 +0530

    Words count

--- java.java
+++ java.java
@@ -42,6 +42,35 @@ public class StringSegregation {
 
 
 
+
+
+
+
+
+
+
+package Stringprograms;
+import java.util.Scanner;
+public class WordCount {
+
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the String:");
+        String str = sc.nextLine();
+        str= str.trim();
+        int spc_cnt=0;
+        
+        for(int i=0; i<str.length(); i++) {
+        	if(str.charAt(i)==' ' && str.charAt(i+1)!=' ') {
+        		spc_cnt++;
+        	}
+        }
+        int word_count = spc_cnt+1;
+        System.out.println("Total number of words are:"+ word_count);
+	}
+	}
+
+
 	}
 
 }
